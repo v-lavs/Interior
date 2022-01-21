b7994b0a84476abe329ae308373de6f92b9f313a
v-lavs <lavskavanda@gmail.com>
2022-01-21 22:36:06 +0200

    add members-team

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,8 @@
 * */
 
 //= include ../../node_modules/jquery/dist/jquery.js ;
+//= include ../lib/jquery-ui.js ;
+//= include ../../node_modules/masonry-layout/masonry.js
 //= include ../lib/waypoints/index.js
 //= include ../lib/bg-check.js
 
@@ -10,8 +12,84 @@
 // CUSTOM SCRIPTS
 
 $(document).ready(function () {
-    // MOBILE MENU
+    //COUNTER UP
+    (function ($) {
+        $.fn.countTo = function (options) {
+            options = options || {};
 
+            return $(this).each(function () {
+                // set options for current element
+                var settings = $.extend({}, $.fn.countTo.defaults, {
+                    from: $(this).data('from'),
+                    to: $(this).data('to'),
+                    speed: $(this).data('speed'),
+                    refreshInterval: $(this).data('refresh-interval'),
+                    decimals: $(this).data('decimals')
+                }, options);
+
+                var loops = Math.ceil(settings.speed / settings.refreshInterval),
+                    increment = (settings.to - settings.from) / loops;
+
+                var self = this,
+                    $self = $(this),
+                    loopCount = 0,
+                    value = settings.from,
+                    data = $self.data('countTo') || {};
+
+                $self.data('countTo', data);
+
+                if (data.interval) {
+                    clearInterval(data.interval);
+                }
+                data.interval = setInterval(updateTimer, settings.refreshInterval);
+
+                render(value);
+
+                function updateTimer() {
+                    value += increment;
+                    loopCount++;
+
+                    render(value);
+
+                    if (typeof (settings.onUpdate) == 'function') {
+                        settings.onUpdate.call(self, value);
+                    }
+
+                    if (loopCount >= loops) {
+                        $self.removeData('countTo');
+                        clearInterval(data.interval);
+                        value = settings.to;
+
+                        if (typeof (settings.onComplete) == 'function') {
+                            settings.onComplete.call(self, value);
+                        }
+                    }
+                }
+
+                function render(value) {
+                    var formattedValue = settings.formatter.call(self, value, settings);
+                    $self.html(formattedValue);
+                }
+            });
+        };
+
+        $.fn.countTo.defaults = {
+            from: 0,
+            to: 0,
+            speed: 1000,
+            refreshInterval: 100,
+            decimals: 0,
+            formatter: formatter,
+            onUpdate: null,
+            onComplete: null
+        };
+
+        function formatter(value, settings) {
+            return value.toFixed(settings.decimals);
+        }
+    }(jQuery));
+
+    // MOBILE MENU
     const nav = $('.header-nav');
 
     $('.btn-burger').on('click', function (e) {
@@ -54,7 +132,7 @@ $(document).ready(function () {
     // BANNER SLIDER
 
     if ($('.banner-slider').length > 0) {
-        const homeBanner = new Swiper('.banner-slider',  {
+        const homeBanner = new Swiper('.banner-slider', {
             loop: true,
             speed: 2000,
             autoplay: {
@@ -66,8 +144,29 @@ $(document).ready(function () {
         });
     }
 
+    // SLIDER SOLUTION
+    let sliderSolution = new Swiper("#sliderSolution", {
+        pagination: {
+            el: ".swiper-pagination",
+        },
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
 
-      //STICKY BTN
+
+    });
+
+    //  SLIDER PROJECTS
+    let sliderProject = new Swiper("#sliderProject", {
+        autoplay: {
+            delay: 2000,
+        },
+        loop: true,
+        speed: 2000,
+    });
+
+    //STICKY BTN
     var sticky = new Waypoint({
         element: $('#triggerBtnSticky')[0],
         offset: '50%',
@@ -89,6 +188,28 @@ $(document).ready(function () {
         $(this).hide();
     });
 
+
+    // HOVER BLOCK
+    const $memberItems = $('#membersTeams .members-team__item');
+
+    const sliderMembers = new Swiper('#sliderMembers', {
+        slidesPerView: 1,
+        loop: false,
+        effect: 'fade',
+        spaceBetween: 10,
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+    });
+
+    $memberItems.hover(function () {
+        $memberItems.removeClass('active');
+        $(this).addClass('active');
+        const index = $(this).index();
+        sliderMembers.slideTo(index);
+    });
+
     //ANIMATIOON
 
     setTimeout(function () {
@@ -98,5 +219,35 @@ $(document).ready(function () {
             offset: '75%'
         });
     }, 300);
+
+    // COUNTER
+
+    $('.achievements').waypoint(function (direction) {
+        jQuery(function ($) {
+            // custom formatting example
+            $('.count-number').data('countToOptions', {
+                formatter: function (value, options) {
+                    return value.toFixed(options.decimals).replace(/\B(?=(?:\d{3})+(?!\d))/g, ',');
+                }
+            });
+
+            // start all the timers
+            $('.timer').each(count);
+
+            function count(options) {
+                var $this = $(this);
+                options = $.extend({}, options || {}, $this.data('countToOptions') || {});
+                $this.countTo(options);
+            }
+        });
+    }, {
+        offset: '100%'
+    });
+
+    $('.gallery').masonry({
+        // options
+        itemSelector: '.gallery__item',
+        columnWidth: 200
+    });
 
 });
